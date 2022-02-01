01010e0d1905373cb80e78a7ab6db69a0c8e26b6
Nikolaj Støvring <nstovring@live.dk>
2022-02-01 13:55:40 +0100

    Initial Commit

---
+++ Assets/ComputeShaderTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ComputeShaderTest : MonoBehaviour
+{
+    public ComputeShader compute_shader;
+    public RenderTexture render_texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        render_texture = new RenderTexture(256,256,1);
+        render_texture.enableRandomWrite = true;
+        render_texture.Create();
+
+        compute_shader.SetTexture(0, "Result", render_texture);
+        compute_shader.Dispatch(0,render_texture.width/8, render_texture.height/8, 1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest){
+        if(render_texture == null){
+             render_texture = new RenderTexture(256,256,1);
+            render_texture.enableRandomWrite = true;
+            render_texture.Create();
+        }
+
+        compute_shader.SetTexture(0, "Result", render_texture);
+        compute_shader.Dispatch(0,render_texture.width/8, render_texture.height/8, 1);
+
+        Graphics.Blit(render_texture, dest);
+    }
+
+}
