188ae95ea561c15cec2260953e70d223704d0e52
Nikolaj Støvring <nstovring@live.dk>
2022-02-07 12:06:32 +0100

    Basic Diffusion

--- Assets/ComputeShaderTest.cs
+++ Assets/ComputeShaderTest.cs
@@ -7,23 +7,57 @@ public class ComputeShaderTest : MonoBehaviour
 {
     public ComputeShader compute_shader;
     public RenderTexture render_texture;
+    public RenderTexture dye_texture;
+
+    public RenderTexture velocity_texture;
+
+
+    int SetColorWhiteKernelIndex = 1;
+    int MainKernelIndex = 0;
+
+    [Range(-20f,20f)]
+    public float diffusion_factor = 1;
+
+    public float sinPow = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        render_texture = new RenderTexture(256,256,1);
-        render_texture.enableRandomWrite = true;
-        render_texture.Create();
-
-        compute_shader.SetTexture(0, "Result", render_texture);
-        compute_shader.Dispatch(0,render_texture.width/8, render_texture.height/8, 1);
+        InitTexture(out render_texture);
+        InitTexture(out velocity_texture);
+        InitTexture(out dye_texture);
 
 
+        compute_shader.SetTexture(SetColorWhiteKernelIndex, "Result", render_texture);
+        compute_shader.SetTexture(SetColorWhiteKernelIndex, "Dye", dye_texture);
+
+        compute_shader.SetTexture(MainKernelIndex, "Result", render_texture);
+        compute_shader.SetTexture(MainKernelIndex, "Dye", dye_texture);
+
+
+        compute_shader.Dispatch(SetColorWhiteKernelIndex,render_texture.width/8, render_texture.height/8, 1);
+
+
+    }
+
+
+
+    void InitTexture(out RenderTexture r)
+    {
+        r = new RenderTexture(256, 256, 1);
+        r.enableRandomWrite = true;
+        r.Create();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //diffusion_factor += Mathf.Sin(Time.time) * sinPow;
+
+        if (Input.GetMouseButton(0))
+        {
+            compute_shader.Dispatch(SetColorWhiteKernelIndex, render_texture.width / 8, render_texture.height / 8, 1);
+        }
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest){
@@ -33,10 +67,20 @@ public class ComputeShaderTest : MonoBehaviour
             render_texture.Create();
         }
 
-        compute_shader.SetTexture(0, "Result", render_texture);
-        compute_shader.Dispatch(0,render_texture.width/8, render_texture.height/8, 1);
+        //compute_shader.SetTexture(MainKernelIndex, "Result", render_texture);
+        compute_shader.SetFloat("deltaTime", Time.deltaTime);
+        compute_shader.SetFloat("diffusion_factor", diffusion_factor);
+
+        compute_shader.Dispatch(MainKernelIndex,render_texture.width/8, render_texture.height/8, 1);
 
         Graphics.Blit(render_texture, dest);
     }
+
+    void OnDisable(){
+        render_texture.Release();
+        dye_texture.Release();
+        velocity_texture.Release();
+    }
+
 
 }
